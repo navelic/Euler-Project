125a9526702fc5c1150b6964cdee87fe074914f9
navelic <41116230+navelic@users.noreply.github.com>
2018-08-15 13:16:55 +0530

    Create prog2.java

---
+++ prog2.java
@@ -0,0 +1,16 @@
+public class fibo
+{
+	public static void main(String args[])
+	{
+		int a=1,b=2,c=0,d=2;
+		while(c<400000)
+		{
+			c=a+b;
+			a=b;
+			b=c;
+			if(c%2==0)
+				d=d+c;
+		}
+		System.out.println(d);
+		}
+	}
