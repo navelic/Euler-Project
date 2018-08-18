eb458b34d3a281197da39599bc39dcfe540abbdf
Naveli Choudhary <41116230+navelic@users.noreply.github.com>
2018-08-18 22:23:14 +0530

    Update prog2.java

--- prog2.java
+++ prog2.java
@@ -3,13 +3,15 @@ public class fibo
 	public static void main(String args[])
 	{
 		int a=1,b=2,c=0,d=2;
-		while(c<4000000)
+		while(c<=4000000)
 		{
 			c=a+b;
 			a=b;
 			b=c;
 			if(c%2==0)
+			{
 				d=d+c;
+			}
 		}
 		System.out.println(d);
 		}
