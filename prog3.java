5b2e45385d993e3843442a068fcbc3b7cc21e86e
Naveli Choudhary <41116230+navelic@users.noreply.github.com>
2018-09-02 23:36:39 +0530

    Update prog3.java

--- prog3.java
+++ prog3.java
@@ -3,14 +3,16 @@ public class prog3 {
 	public static void main(String[] args) {
 		 int i;
 		 long num=600851475143l;
+			 while(num%2==0)
+				 num=num/2;
+		        for (i=3;i<=Math.sqrt(num);i+=2) 
+		        {
+		            if (num%i==0)
+		            {
+		                num=num/i;
+		            }
+		        }
+		        if(num>2)
+		        System.out.println(num);
+		}}
 
-	        for (i=2;i<=num;i++) 
-	        {
-	            if (num%i==0)
-	            {
-	                num=num/i;
-	                i--;
-	            }
-	        }
-	        System.out.println(i);
-	}}
